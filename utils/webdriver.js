8242dfc3742e94fee7a4a4b9c27bd65af60c7d3f
Mykhailo <mr.mike220011@gmail.com>
2023-09-15 18:50:04 +0300

    add headless mode

--- utils/webdriver.js
+++ utils/webdriver.js
@@ -1,9 +1,11 @@
 import {Builder, By, until} from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome.js';
+import firefox from 'selenium-webdriver/firefox.js';
 import config from '../config/config.js';
 
 export default new (class WebDriver {
     constructor() {
-        this.driver = new Builder().forBrowser(config.browser).build();
+        this.driver = new Builder().forBrowser(config.browser).setChromeOptions(new chrome.Options().headless()).setFirefoxOptions(new firefox.Options().headless()).build();
         this.driver.manage().window().maximize();
         this.driver.manage().setTimeouts({implicit: config.timeout});
     }
